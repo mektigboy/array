16d40541ca52893d04425f319b01f5b435a45108
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-30 13:46:36 -0500

    Update Array.sol

--- Array.sol
+++ Array.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.8.14;
 
 contract Array {
-  uint[] public nums = [1, 2, 3];
-  uint[3] public numsFixed = [4, 5, 6];
+    uint[] public nums = [1, 2, 3];
+    uint[3] public numsFixed = [4, 5, 6];
+    
+    function examples() external {
+        nums.push(4);
+        uint x = nums[1];
+        nums[2] = 777;
+        delete nums[1];
+        nums.pop();
+        uint len = nums.length;
 
-  function examples() external {
-    nums.push(4);
-    uint x = nums[1];
-    nums[2] = 777;
-    delete nums[1];
-    nums.pop();
-    uint len = nums.length;
-
-    // Create array in memory.
-    uint[] memory a = new uint[](5);
-    a[1] = 123;
-  }
-  function returnArray() external view returns (uint[] memory) {
-    return nums;
-  }
+        // Create array in memory.
+        uint[] memory a = new uint[](5);
+        a[1] = 123;
+    }
+    function returnArray() external view returns (uint[] memory) {
+        return nums;
+    }
 }
